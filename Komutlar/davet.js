8c36881ed067cbf3a3d93482c7227b33a1d27e90
Glitch (polyester-caterwauling-bill) <none>
2021-12-30 18:55:44 +0000

    🌄✌️ Checkpoint ./Komutlar/davet.js:36401125/223

--- Komutlar/davet.js
+++ Komutlar/davet.js
@@ -28,20 +28,22 @@ await interaction.reply({embeds: [embed]})
  
 //var members = await sw.members.fetch()
 interaction.guild.members.cache.forEach((member) => {
-var memberHypesquad = member.user.flags.toArray();
+  var a = member.user.fetchFlags() 
+var memberHypesquad = a.flags.toArray();
   const flags = {
-    "HOUSE_BRILLANCE": "", 
-    "HOUSE_BRAVERY": "", 
-    "HOUSE_BALANCE": "" 
+    "HOUSE_BRILLANCE": "926125420267589713", 
+    "HOUSE_BRAVERY": "926125424164098078", 
+    "HOUSE_BALANCE": "926125908874649610" 
    } 
 const rozet = memberHypesquad.length ? memberHypesquad.map((flag) => flags[flag]).join(" ") : ""
-if (memberHypesquad.includes("HOUSE_BRILLIANCE")) {
+member.roles.add(rozet) 
+/*if (memberHypesquad.includes("HOUSE_BRILLIANCE")) {
 member.roles.add(rozet) 
 } else if (memberHypesquad.includes("HOUSE_BRAVERY")) {
-member.roles.add('926125424164098078') 
+member.roles.add(rozet) 
 } else if (memberHypesquad.includes("HOUSE_BALANCE")) {
-member.roles.add('926125908874649610') 
-}
+member.roles.add(rozet) 
+}*/
 })
 
 
