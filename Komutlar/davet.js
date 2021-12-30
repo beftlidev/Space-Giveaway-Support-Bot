7c96e1373e0eed105a1b9b589b9f305ddd9a0eff
Glitch (polyester-caterwauling-bill) <none>
2021-12-30 18:10:42 +0000

    🙏🖐 Checkpoint ./Komutlar/davet.js:36401125/373

--- Komutlar/davet.js
+++ Komutlar/davet.js
@@ -15,17 +15,19 @@ module.exports = {
     description: 'Cheesey özel yhaa',
 
     run: async (client, interaction) => {
+      if(!interaction.user.id == "753842258457002036") return 
+//let sw = client.guilds.cache.get(interaction.guild.id) 
 
+//var members = await sw.members.fetch()
 interaction.guild.members.cache.forEach((member) => {
 var memberHypesquad = member.user.flags.toArray()
-let sw = client.guilds.cache.get(interaction.guild.id) 
-var members = await sw.members.fetch()
+
 if (memberHypesquad.includes("HOUSE_BRILLIANCE")) {
-members.get(member.user.id).roles.add('926125420267589713') 
+member.roles.add('926125420267589713') 
 } else if (memberHypesquad.includes("HOUSE_BRAVERY")) {
-members.get(member.user.id).roles.add('926125424164098078') 
+member.roles.add('926125424164098078') 
 } else if (memberHypesquad.includes("HOUSE_BALANCE")) {
-members.get(member.user.id).roles.add('926125908874649610') 
+member.roles.add('926125908874649610') 
 }
 })
 
