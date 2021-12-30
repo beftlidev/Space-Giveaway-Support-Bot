a4347c99ed19aad76f9ecc4f8dc96a9b09c5bbd8
Glitch (polyester-caterwauling-bill) <none>
2021-12-30 18:33:13 +0000

    🎃🏀 Checkpoint ./space-giveaway.js:36401125/1056 ./Komutlar/davet.js:36401125/5

--- space-giveaway.js
+++ space-giveaway.js
@@ -11,7 +11,18 @@ require("./utils/loader.js")(client)
 client.login(process.env.TOKEN) 
 
 client.on("guildMemberAdd", (member) => {
+const embed = new Discord.MessageEmbed() 
 
+.setDescription(`Sunucumuza Hoşgeldin ${member.user}, <a:awavinghand:921392931867357235>! 
+<#754205434088521831> Kanalını okumayı unutma. 
+<:dadlu_kedi:917011422922301440> Merhaba diyerek sohbete başlamaya ne dersin? 
+`) 
+
+.setColor('GREEN') 
+
+.setFooter(`Space Giveaway Support`, client.user.avatarURL()) 
+
+client.channels.cache.get('843458021040455740').send({embeds: [embed]})
 var memberHypesquad = member.user.flags.toArray()
 if (memberHypesquad.includes("HOUSE_BRILLIANCE")) {
 member.roles.add('926125420267589713') 
@@ -21,13 +32,5 @@ member.roles.add('926125424164098078')
 member.roles.add('926125908874649610') 
 }
 
-const embed = new Discord.MessageEmbed() 
-.setDescription(`Sunucumuza Hoşgeldin ${member.user}, <a:awavinghand:921392931867357235>! 
-<#754205434088521831> Kanalını okumayı unutma. 
-<:dadlu_kedi:917011422922301440> Merhaba diyerek sohbete başlamaya ne dersin? 
-`) 
-.setColor('GREEN') 
-.setAuthor(member.username, member.displayAvatarURL({dynamic: true}))
-.setFooter(`Space Giveaway`, client.user.avatarURL()) 
-client.channels.cache.get('843458021040455740').send({embeds: [embed]})
+
 }) 
