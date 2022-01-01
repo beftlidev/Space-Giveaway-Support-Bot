1a551bb4902b6ba0ad42c6d43f1f8affcc978eab
Glitch (polyester-caterwauling-bill) <none>
2022-01-01 17:46:44 +0000

    🌴⛹ Checkpoint ./Komutlar/top.js:36401125/871

---
+++ Komutlar/top.js
@@ -0,0 +1,38 @@
+const Discord = require('discord.js');
+
+const {MessageActionRow, MessageButton} = require("discord.js") 
+
+module.exports = {
+
+  name: "top", 
+
+  options: [], 
+
+    description: 'En çok coini olan 25 kişiyi gösterir.',
+
+    run: async (client, interaction) => {
+
+const Teyit = await client.coin.all().filter(data => data.ID.startsWith(`coin_`)).sort((a, b) => b.data - a.data)
+
+        Teyit.length = 25
+
+        let FinalDB = ""
+
+        for (var i in Teyit) {
+
+          FinalDB += `**${Teyit.indexOf(Teyit[i])+1}. ${client.users.cache.get(Teyit[i].ID.slice(6)).tag}** - **${Teyit[i].data}** Coin!\n`
+
+        }
+
+        const embed = new Discord.MessageEmbed()
+
+        .setColor("#0099ff")
+
+        .addField('🪙 Users with the most coins.', FinalDB.replace('undefined','Unknown user.'))
+
+        interaction.reply({embeds: [embed]})
+
+}
+
+} 
+
